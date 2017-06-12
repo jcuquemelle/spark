b141f7c2f367a4f1359dc0003322fcd9f22351a9
Sergei Lebedev <superbobry@gmail.com>
2017-06-12 12:54:38 +0200

    Revert "Selenium test fix"

--- core/src/test/scala/org/apache/spark/deploy/history/HistoryServerSuite.scala
+++ core/src/test/scala/org/apache/spark/deploy/history/HistoryServerSuite.scala
@@ -26,7 +26,6 @@ import scala.concurrent.duration._
 import scala.language.postfixOps
 
 import com.codahale.metrics.Counter
-import com.gargoylesoftware.htmlunit.BrowserVersion
 import com.google.common.io.{ByteStreams, Files}
 import org.apache.commons.io.{FileUtils, IOUtils}
 import org.apache.hadoop.fs.{FileStatus, FileSystem, Path}
@@ -318,7 +317,7 @@ class HistoryServerSuite extends SparkFunSuite with BeforeAndAfter with Matchers
     contextHandler.addServlet(holder, "/")
     server.attachHandler(contextHandler)
 
-    implicit val webDriver: WebDriver = new HtmlUnitDriver(BrowserVersion.CHROME, true) {
+    implicit val webDriver: WebDriver = new HtmlUnitDriver(true) {
       getWebClient.getOptions.setThrowExceptionOnScriptError(false)
     }
 
